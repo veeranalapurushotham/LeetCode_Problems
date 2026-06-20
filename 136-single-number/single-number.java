af0dcb29e83f6efda9836b978633c3f87012cbc9
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-06-20 21:53:16 +0530

    Time: 1 ms (99.97%) | Memory: 46.9 MB (45.15%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,26 +1,31 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int count=0;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++)
+        // {
+        //     if(nums[i]!=nums[i+1])
+        //     {
+        //         if(count==0)
+        //         {
+        //             return nums[i];
+        //         }
+        //         else
+        //         {
+        //             count=0;
+        //             continue;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         count++;
+        //     }
+        // }
+        // return nums[nums.length-1];
+        for(int i:nums)
         {
-            if(nums[i]!=nums[i+1])
-            {
-                if(count==0)
-                {
-                    return nums[i];
-                }
-                else
-                {
-                    count=0;
-                    continue;
-                }
-            }
-            else
-            {
-                count++;
-            }
+            count^=i;
         }
-        return nums[nums.length-1];
+        return count;
     }
 }
