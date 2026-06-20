aa4e71fc8120cf048a3e05501a7cf76c95820ef5
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-06-20 21:41:14 +0530

    Time: 11 ms (23.21%) | Memory: 47.6 MB (7.19%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                if(count==0)
+                {
+                    return nums[i];
+                }
+                else
+                {
+                    count=0;
+                    continue;
+                }
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
