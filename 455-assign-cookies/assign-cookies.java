16faea3d11700d6a0aad48e21c5583148e51b2cf
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-06-25 11:39:13 +0530

    Time: 15 ms (62.12%) | Memory: 51.6 MB (36.67%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(i<g.length && j<s.length)
+        {
+            if(g[i]<=s[j])
+            {
+                cnt++;
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+            
+        }
+        return cnt;
+    }
+}
