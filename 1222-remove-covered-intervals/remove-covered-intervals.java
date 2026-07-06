92a488793d0a853d699c2d7984d7b1cdc3433880
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-07-06 11:09:42 +0530

    Time: 6 ms (93.21%) | Memory: 46.5 MB (43.52%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,23 +1,26 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-         Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
-         int count=0;
-         for(int i=0;i<intervals.length;i++)
-         {
-            int[] cur=intervals[i];
-            for(int j=0;j<intervals.length;j++)
+         Arrays.sort(intervals,(a,b)->{
+            if(a[0]!=b[0])
             {
-                if(j!=i)
-                {
-                    int[] sec=intervals[j];
-                    if(cur[0]>=sec[0] && cur[1]<=sec[1])
-                    {
-                        count=count+1;
-                        break;
-                    }
-                }
+                return a[0] - b[0];
             }
-         }
-         return intervals.length-count;
+            return b[1] - a[1];
+         });
+        int l=intervals.length;
+        int low=intervals[0][0];
+        int high=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]==low || intervals[i][1]<=high)
+            {
+                l--;
+            }
+            else
+            {
+                low=intervals[i][0];
+                high=intervals[i][1];
+            }
+        }
+        return l;
     }
 }
