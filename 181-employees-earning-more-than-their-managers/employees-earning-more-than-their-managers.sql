d81948a94bee7882129354194de90be19825ffa9
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-07-12 23:47:18 +0530

    Time: 394 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,3 +1,3 @@
 /* Write your T-SQL query statement below */
-select e.name as Employee from Employee e where e.salary
->(select m.salary  from Employee m where m.id=e.managerId)
+select e.name as Employee from Employee e left join Employee m on e.managerId=m.id where 
+e.salary>m.salary
