cb4d0920cf4ae1dbb1ae0bf66aa3f2943f77f8eb
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-07-15 16:14:35 +0530

    Time: 1 ms (83.11%) | Memory: 42.8 MB (6.12%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,25 +1,26 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumod=0;
-        int sumev=0;
-        for(int i=1;i<=2*n;i++)
-        {
-            if(i%2==0)
-            {
-                sumev+=i;
-            }
-            else
-            {
-                sumod+=i;
-            }
-        }
-        while(sumod!=0)
-        {
-            int temp=sumod;
-            sumod=sumev%sumod;
-            sumev=temp;
+        // int sumod=0;
+        // int sumev=0;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(i%2==0)
+        //     {
+        //         sumev+=i;
+        //     }
+        //     else
+        //     {
+        //         sumod+=i;
+        //     }
+        // }
+        // while(sumod!=0)
+        // {
+        //     int temp=sumod;
+        //     sumod=sumev%sumod;
+        //     sumev=temp;
             
-        }
-        return  sumev;
+        // }
+        // return  sumev;
+        return n;
     }
 }
