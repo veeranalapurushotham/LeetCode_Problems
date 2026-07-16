400a7dfc5665f237d4e5710d4e7b5054ec50a3a0
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-07-16 16:06:03 +0530

    Time: 57 ms (49.50%) | Memory: 108.1 MB (53.00%) - LeetSync

---
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            mx = Math.max(mx, nums[i]);
+            prefixGcd[i] = gcd(nums[i], mx);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long ans = 0;
+        for (int i = 0; i < n / 2; i++) {
+            ans += gcd(prefixGcd[i], prefixGcd[n - 1 - i]);
+        }
+
+        return ans;
+    }
+}
