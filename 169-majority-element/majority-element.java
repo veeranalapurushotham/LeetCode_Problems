2f129485f4a3e5d0f6dd187890c0d15b1ff0b162
Veeranala Purushotham <234g1a33c8@srit.ac.in>
2026-08-15 17:06:19 +0530

    Time: 1 ms (99.89%) | Memory: 55.9 MB (6.79%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,27 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        if(nums.length<2)
+        int last=0;
+        int cnt=0;
+        for(int i:nums)
         {
-            return nums[0];
-        }
-        int t=nums.length/2;
-        Arrays.sort(nums);
-        int cnt=1;
-        for(int i=1;i<nums.length;i++)
-        {
-            if(cnt>t)
+            if(cnt==0)
             {
-                return nums[i-1];
-            }
-            if(nums[i-1]==nums[i])
+                last=i;
+                cnt=1;
+                continue;
+            }   
+            if(last==i)
             {
                 cnt++;
             }
             else
             {
-                cnt=1;
+                cnt--;
             }
         }
-        return nums[nums.length-1];
+        return last;
     }
 }
